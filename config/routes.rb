4baa38077a89043c9d2b2c5b9f424ac6d28851e0
吉永稔弘 <yoshinaga@yoshinagaminoruhiroshinoMacBook-Pro.local>
2018-08-31 18:25:57 +0900

    fix pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,9 @@ Rails.application.routes.draw do
   get 'contract' => "home#contract"
   get 'contract_new' => "home#contract_new"
   get 'contract_list' => "home#contract_list"
-  get 'payback_new' => "home#payback_new"
-  get 'payback_agree' => "home#payback_agree"
-  get 'friend_new' => "home#friend_new"
-  get 'friend_list' => "home#friend_list"
-  post "friends" => "home#addFriend"
-  post "payments" => "home#createPayment"
+  get 'contract_agree' => "home#contract_agree"
+  get 'contract_complete' => "home#contract_complete"
   post "contracts" => "home#createContract"
-  post "agreePayment" => "home#agreePayment"
-  post "disagreePayment" => "home#disagreePayment"
+  post "payments" => "home#createPayment"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20180815164614_create_contracts.rb
+++ db/migrate/20180815164614_create_contracts.rb
@@ -1,8 +1,8 @@
 class CreateContracts < ActiveRecord::Migration[5.2]
   def change
     create_table :contracts do |t|
-      t.text :debit
-      t.text :credit
+      t.bigint :friend_id
+      t.bigint :user_id
       t.integer :amount
       t.text :status
       t.text :note
