39cc7e1f804d40e28ba1efc823ca4cd70354c332
koichirosawada <sawadakoichiro@gmail.com>
2018-09-30 13:45:36 +0900

    changeDisplayOfContract

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -9,3 +9,30 @@ module HomeHelper
     return ["UNREAD","PAID"]
   end
 end
+
+module HomeHelper
+  def getFriendName
+    friendName=Friend.find(@contract.friend_id)
+    return friendName.name
+  end
+end
+
+module HomeHelper
+  def getContractName
+    contractName=User.find(@contract.user_id)
+    return contractName.name
+  end
+end
+
+module HomeHelper
+  def getContractStatus
+    if @contract.status == "UNREAD"
+      statusName = "未決済"
+    elsif @contract.status == "PAID"
+      statusName = "決済済み"
+    else
+      statusName = "削除済み"
+    end
+      return statusName
+  end
+end
